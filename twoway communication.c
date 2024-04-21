0f5e9772b8aa342a054e436902170fdbdd934ddf
pdbala <151711011+pdbala@users.noreply.github.com>
2024-04-22 00:09:39 +0530

    Update twoway communication.c

--- twoway communication.c
+++ twoway communication.c
@@ -8,34 +8,52 @@ int main() {
    char pipe1writemessage[20] = "Hi";
    char pipe2writemessage[20] = "Hello";
    char readmessage[20];
-   returnstatus1 = pipe(pipefds1);
 
+   returnstatus1 = pipe(pipefds1);
    if (returnstatus1 == -1) {
       printf("Unable to create pipe 1 \n");
       return 1;
    }
-   returnstatus2 = pipe(pipefds2);
 
+   returnstatus2 = pipe(pipefds2);
    if (returnstatus2 == -1) {
       printf("Unable to create pipe 2 \n");
       return 1;
    }
+
    pid = fork();
 
-   if (pid != 0) // Parent process {
+   if (pid != 0) { // Parent process
       close(pipefds1[0]); // Close the unwanted pipe1 read side
       close(pipefds2[1]); // Close the unwanted pipe2 write side
-      printf("In Parent: Writing to pipe 1 – Message is %s\n", pipe1writemessage);
-      write(pipefds1[1], pipe1writemessage, sizeof(pipe1writemessage));
-      read(pipefds2[0], readmessage, sizeof(readmessage));
+
+      if(write(pipefds1[1], pipe1writemessage, sizeof(pipe1writemessage)) == -1){
+         printf("Error writing to pipe 1\n");
+         return 1;
+      }
+
+      if(read(pipefds2[0], readmessage, sizeof(readmessage)) == -1){
+         printf("Error reading from pipe 2\n");
+         return 1;
+      }
+
       printf("In Parent: Reading from pipe 2 – Message is %s\n", readmessage);
-   } else { //child process
+   } else { // Child process
       close(pipefds1[1]); // Close the unwanted pipe1 write side
       close(pipefds2[0]); // Close the unwanted pipe2 read side
-      read(pipefds1[0], readmessage, sizeof(readmessage));
+
+      if(read(pipefds1[0], readmessage, sizeof(readmessage)) == -1){
+         printf("Error reading from pipe 1\n");
+         return 1;
+      }
+
       printf("In Child: Reading from pipe 1 – Message is %s\n", readmessage);
-      printf("In Child: Writing to pipe 2 – Message is %s\n", pipe2writemessage);
-      write(pipefds2[1], pipe2writemessage, sizeof(pipe2writemessage));
+
+      if(write(pipefds2[1], pipe2writemessage, sizeof(pipe2writemessage)) == -1){
+         printf("Error writing to pipe 2\n");
+         return 1;
+      }
    }
+
    return 0;
 }
